1e897664694badd5df7c6b6cc14d244d6c8a5a03
srinivas <srinivasiyerb@gmail.com>
2013-12-13 17:34:29 +0530

    Update NewProduct.java

--- src/com/rl/test/selenium/NewProduct.java
+++ src/com/rl/test/selenium/NewProduct.java
@@ -7,7 +7,7 @@ public class NewProduct extends MainDriver{
 	public static void ProductList(){
 				
 		driver.findElement(By.xpath("//a[contains(text(),'product list')]")).click();
-		driver.findElement(By.linkText("hme")).click();
+		driver.findElement(By.linkText("h0me")).click();
 	}
 
 }
