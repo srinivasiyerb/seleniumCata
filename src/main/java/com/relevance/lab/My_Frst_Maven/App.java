b9a2b7ca6145078734e3fedb7cf1ec512cfdb271
srini <srinivasiyerb@gmail.com>
2014-01-20 18:08:51 +0530

    test

--- src/main/java/com/relevance/lab/My_Frst_Maven/App.java
+++ src/main/java/com/relevance/lab/My_Frst_Maven/App.java
@@ -5,13 +5,13 @@ package com.relevance.lab.My_Frst_Maven;
  * Hello world!
  *
  */
-public class App 
+public class App extends main 
 {
 	
 	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println( "hi jenkins!" );
-        System.out.println( "hi jenkins!" );
+      
     }
 }
---
+++ src/main/java/com/relevance/lab/My_Frst_Maven/main.java
@@ -0,0 +1,17 @@
+package com.relevance.lab.My_Frst_Maven;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class main 
+{
+	
+	public static void test( )
+    {
+       
+      System.out.println( "test!" );
+	  System.out.println( "test!" );
+    }
+}
